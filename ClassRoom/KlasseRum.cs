edc3beae16c9e68998ccb8158cbb23a47e5775c9
felex <felicxmose@msn.com>
2019-02-01 17:49:18 +0100

    opgave2 fuldført

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        private string klasseNavn;
+        private List<Studerende> klasseliste;
+        private DateTime SemesterStart;
+
+        public KlasseRum()
+        {
+
+        }
+    }
+}
