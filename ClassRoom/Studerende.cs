2a4865dddd52eabe15239c4abd27d6bc0d4f95c2
felex <felicxmose@msn.com>
2019-02-01 17:41:53 +0100

    opgave1 fuldført

---
+++ ClassRoom/Studerende.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Studerende
+    {
+        public string Navn;
+        public int fødselsmåned;
+        public int fødselsdag;
+
+        public Studerende(string name, int fødselsmåned, int fødselsdag )
+        {
+            this.Navn = name;
+            this.fødselsmåned = fødselsmåned;
+            this.fødselsdag = fødselsdag;
+        }
+    }
+}
