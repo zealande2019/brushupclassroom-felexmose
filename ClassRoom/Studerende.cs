46c09e53c367d6f99ec658299078f8f259e90792
felex <felicxmose@msn.com>
2019-02-02 12:34:49 +0100

    opgave7 add a contraint on a contructor paremeter, is still challenging. see line 18 in class studerende

--- ClassRoom/Studerende.cs
+++ ClassRoom/Studerende.cs
@@ -15,6 +15,7 @@ namespace ClassRoom
 
         public Studerende(string name, int fødselsmåned, int fødselsdag )
         {
+            // adding a constraint or requirement on a constructor parameter?
             // for at sørger for at der indtastes te tal mellem 1-12 som fødselsmåned
             // kræver det premium bruger, kode er såledels : Contract.Requires(fødselsmåned<12 && fødselsmåned >1);
 
